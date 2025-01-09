d1a2bc7d6012408268c6459533dfbd8fe4b7bb8f
zaczac117 <zacen117@gmail.com>
2025-01-10 05:49:11 +0900

    first commit

---
+++ src/main/java/com/example/BookManagementApp/BookManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookManagementApp/BookManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
